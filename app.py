0cf569cb98eddc5fae482a03f54a9390ae891a71
=Wajuma <=meltusjuma5@gmail.com>
2021-10-07 22:20:37 +0300

    Get Card Vendor
    Implemented get_vendor method
    The app can now retrieve the card vendor from the card number

---
+++ app.py
@@ -0,0 +1,32 @@
+class CreditCard:
+	def __init__(self, card_no):
+		self.card_no =card_no
+
+	@classmethod
+	def get_vendor(self):
+		vendor =None
+		crn =str(self.card_no)
+		if crn.startswith('5'):
+			vendor ='Visa Card' 
+		elif crn.startswith(('57', '68', '73')):
+			vendor ='Maestro Card'
+		elif crn.startswith('50'):
+			vendor ='Master Card'
+		elif crn.startswith('7'):
+			vendor ='American Express'
+		elif crn.startswith('43'):
+			vendor ='UnionPay Card'
+
+		return vendor
+	
+if __name__ == '__main__':
+	print("[+] Enter card number: ", end ='')
+	card_no =int(input())
+	ccv =CreditCard(card_no)
+	vendor =ccv.get_vendor()
+	print(f"[+] Card Number: {card_no}")
+	if vendor:
+		print(f"[+] Card Vendor: {vendor}")
+
+	else:
+		print(f"[-] Unknown card vendor!")
